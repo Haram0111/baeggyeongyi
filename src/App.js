a58483356527c1c16eb91218d2b69b6bf9d128f3
Haram0111 <5gkfka5@naver.com>
2022-08-24 19:39:32 +0900

    Main page Wideframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
+import styled from "styled-components";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass, faAlignJustify } from "@fortawesome/free-solid-svg-icons";
 import './App.css';
+
+const StyledNav = styled.div`
+  height: 150px;
+  width: 100%;
+  background-color: blue;
+  `;
+
+const StyledAdv = styled.div`
+  float: left;
+  margin: 10px 0 0 5%;
+  height: 100px;
+  width: 20%;
+  background-color: #61dafb;
+  `;
+
+const StyledContanier = styled.div`
+  float: left;
+  margin: 10px 0 0 10px;
+  height: 500px;
+  width: 50%;
+  background-color: red;
+  `;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledNav>
+        <FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: "30px"}} onClick={{}}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faBell} style={{fontSize: "30px"}}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faAlignJustify} style={{fontSize: "30px"}}></FontAwesomeIcon>
+      </StyledNav>
+      <StyledAdv>
+        <p>광고바</p>
+      </StyledAdv>
+      <StyledContanier>
+        Contanier
+      </StyledContanier>
+      
     </div>
   );
 }
