fed1c667892a332b186e05b3c5e30f4aef429665
Haram0111 <5gkfka5@naver.com>
2022-08-29 13:00:11 +0900

    A: like cnt

--- src/Component/postingFeed.jsx
+++ src/Component/postingFeed.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 import feedData from '../Data/feedData';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane, faComments, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import {Col, Row} from 'react-bootstrap'
 import styled from "styled-components";
 
 // const StyledWirterInfo = styled.div`
@@ -23,10 +25,10 @@ function PostingFeed(){
                             <div><img src={a.wirterImg} style={{width: "30px", height: "30px"}}></img>{` `}{a.wirter}</div>
                             <div>{a.date}</div>
                             <div>{a.text}</div>
-                            <div>{a.text}</div>
-                            <FontAwesomeIcon icon={faThumbsUp} style={{fontSize: "10px", color: "white"}}></FontAwesomeIcon>좋아요
-                            <FontAwesomeIcon icon={faComments} style={{fontSize: "10px", color: "white"}}></FontAwesomeIcon>댓글달기
-                            <FontAwesomeIcon icon={faPaperPlane} style={{fontSize: "10px", color: "white"}}></FontAwesomeIcon>공유
+                            <div>좋아요 수 : {a.like}</div>
+                            <FontAwesomeIcon icon={faThumbsUp} style={{fontSize: "10px", color: "white"}}/>좋아요
+                            <FontAwesomeIcon icon={faComments} style={{fontSize: "10px", color: "white"}} ></FontAwesomeIcon>댓글달기
+                            <FontAwesomeIcon icon={faPaperPlane} style={{fontSize: "10px", color: "white"}}/>공유
                         </>
                     )
                 })
