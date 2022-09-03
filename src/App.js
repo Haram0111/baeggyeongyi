ec2eac79d7f2f0b0fb61bd42956a62e9e80bf22a
Haram0111 <5gkfka5@naver.com>
2022-09-03 23:37:26 +0900

    A: Page add

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import SettingBtn from "./Component/setting_btn";
 import Navi_Top from './Component/navi_top';
 import Main from './page/main_page';
 
+
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navi_Top/>
       <Routes>
+        {/* 메인페이지 */}
         <Route path='/' element={<Main/>}/>
+        {/* 1개 feed 보여주는 곳*/}
         <Route path='/:id' element={<Main/>}/>
+        {/* UserPage*/}
+        <Route path='/user/:id' element={<Main/>}/>
+        {/* ChattingPage*/}
+        <Route path='/chatting/:id' element={<Main/>}/>
        {/* 프로필 설정 모달창 - test임 확정 x*/}
         <Route path="/modalTest" element={<SettingBtn/>}/>
       </Routes>
